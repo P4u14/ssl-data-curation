cf31153dcb86e98147fb213e8300d8eeb487d65c
P4u14 <paula.s.schreiber@gmail.com>
2024-09-03 12:07:32 +0200

    add plots

--- countToolAnnotations.py
+++ countToolAnnotations.py
@@ -1,3 +1,4 @@
+import seaborn as sns
 import os
 import re
 import matplotlib.pyplot as plt
@@ -53,25 +54,37 @@ def process_images_file(images_file_path, tool_annotations_folder):
 
     return tool_totals
 
-def plot_tool_counts(tool_counts_over_time, save_path):
+def plot_tool_counts(tool_counts_over_time, save_path, title):
+    # Set the Seaborn theme and context
+    sns.set_theme(style="whitegrid")
+    sns.set_context("poster")  # You can switch to "paper", "poster", "talk", or "notebook" depending on your needs
+    
     percentages = [10 * i for i in range(1, 11)]
     tools = ["Grasper", "Bipolar", "Hook", "Scissors", "Clipper", "Irrigator", "SpecimenBag"]
     
     # Initialize the plot
     plt.figure(figsize=(12, 8))
     
-    # Plot each tool's counts
-    for tool in tools:
+    # Plot each tool's counts with a Seaborn color palette
+    palette = sns.color_palette("deep")  # You can choose different palettes like "muted", "bright", etc.
+    for tool, color in zip(tools, palette):
         counts = [tool_counts.get(tool, 0) for tool_counts in tool_counts_over_time]
-        plt.plot(percentages, counts, marker='o', label=tool)
+        plt.plot(percentages, counts, marker='o', label=tool, color=color)
+
+    # Set y-axis to start at 0
+    plt.ylim(0, None)
     
     # Add labels and title
-    plt.xlabel('Percentage of Images Selected')
-    plt.ylabel('Total Count of Each Tool')
-    plt.title('Tool Counts by Percentage of Images Selected')
+    plt.xlabel('Percentage of images selected')
+    plt.ylabel('Total count of each tool')
+    plt.title(title)
     plt.legend()
     plt.grid(True)
     plt.xticks(percentages)
+
+     # Adjust the layout to add more space for the title and labels
+    plt.tight_layout(pad=2.0)  # pad parameter adds padding between the elements and the edges
+    
     
     # Ensure the destination folder exists
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
@@ -102,37 +115,48 @@ def main():
 
 
 
-    folder = 'random_images_Cholec80/'
+    # folder = 'random_images_Cholec80/'
+    # tool_counts_over_time = []
+    
+    """ for image_file in image_files:
+        images_file_path = os.path.join(folder, image_file)
+        tool_totals = process_images_file(images_file_path, tool_annotations_folder)
+        tool_counts_over_time.append(tool_totals) """
+    
+    # print('tool counts cholec80 random: ')
+    # print(tool_counts_over_time)
+
+    # save_path = 'plots/random_images_Cholec80_presentation.png'
+    # plot_tool_counts(tool_counts_over_time, save_path, 'Random image selection - Tool counts by percentage of images selected')
+
+    # saved_values = [{'Grasper': 7566, 'Bipolar': 651, 'Hook': 8197, 'Scissors': 216, 'Clipper': 431, 'Irrigator': 701, 'SpecimenBag': 807}, {'Grasper': 15014, 'Bipolar': 1260, 'Hook': 16267, 'Scissors': 512, 'Clipper': 854, 'Irrigator': 1391, 'SpecimenBag': 1722}, {'Grasper': 22592, 'Bipolar': 2018, 'Hook': 24384, 'Scissors': 718, 'Clipper': 1253, 'Irrigator': 2106, 'SpecimenBag': 2603}, {'Grasper': 30224, 'Bipolar': 2773, 'Hook': 32365, 'Scissors': 965, 'Clipper': 1768, 'Irrigator': 2771, 'SpecimenBag': 3470}, {'Grasper': 37832, 'Bipolar': 3420, 'Hook': 40562, 'Scissors': 1216, 'Clipper': 2219, 'Irrigator': 3476, 'SpecimenBag': 4198}, {'Grasper': 45355, 'Bipolar': 4045, 'Hook': 48740, 'Scissors': 1453, 'Clipper': 2639, 'Irrigator': 4220, 'SpecimenBag': 5187}, {'Grasper': 52942, 'Bipolar': 4773, 'Hook': 56517, 'Scissors': 1680, 'Clipper': 3094, 'Irrigator': 4890, 'SpecimenBag': 6079}, {'Grasper': 60706, 'Bipolar': 5415, 'Hook': 64921, 'Scissors': 1938, 'Clipper': 3478, 'Irrigator': 5647, 'SpecimenBag': 6857}, {'Grasper': 67975, 'Bipolar': 6157, 'Hook': 72976, 'Scissors': 2183, 'Clipper': 3946, 'Irrigator': 6356, 'SpecimenBag': 7762}, {'Grasper': 75621, 'Bipolar': 6769, 'Hook': 81135, 'Scissors': 2443, 'Clipper': 4399, 'Irrigator': 7007, 'SpecimenBag': 8585}]
+    # plot_tool_counts(saved_values, save_path, 'Distribution of tools in Cholec80 - random image selection')
+
+
+
+
+
+
+    folder = 'dinoV2_Cholec80_no_test_images/'
     tool_counts_over_time = []
     
     for image_file in image_files:
         images_file_path = os.path.join(folder, image_file)
         tool_totals = process_images_file(images_file_path, tool_annotations_folder)
         tool_counts_over_time.append(tool_totals)
+
+    print('tool counts cholec80 dinoV2: ')
+    print(tool_counts_over_time)
     
-    save_path = 'plots/random_images_Cholec80.png'
-    plot_tool_counts(tool_counts_over_time, save_path)
+    save_path = 'plots/dinoV2_Cholec80_no_test_images_presentation.png'
+    plot_tool_counts(tool_counts_over_time, save_path, 'Distribution of tools in Cholec80 - DINOv2')
+
+    saved_values = [{'Grasper': 2594, 'Bipolar': 365, 'Hook': 2697, 'Scissors': 31, 'Clipper': 97, 'Irrigator': 497, 'SpecimenBag': 871}, {'Grasper': 8571, 'Bipolar': 994, 'Hook': 9594, 'Scissors': 237, 'Clipper': 476, 'Irrigator': 1143, 'SpecimenBag': 1738}, {'Grasper': 14186, 'Bipolar': 1643, 'Hook': 16880, 'Scissors': 517, 'Clipper': 887, 'Irrigator': 1751, 'SpecimenBag': 2466}, {'Grasper': 20059, 'Bipolar': 2311, 'Hook': 24333, 'Scissors': 742, 'Clipper': 1370, 'Irrigator': 2336, 'SpecimenBag': 3305}, {'Grasper': 26785, 'Bipolar': 2833, 'Hook': 31457, 'Scissors': 889, 'Clipper': 1706, 'Irrigator': 2823, 'SpecimenBag': 4379}, {'Grasper': 33604, 'Bipolar': 3292, 'Hook': 38981, 'Scissors': 1125, 'Clipper': 2064, 'Irrigator': 3322, 'SpecimenBag': 5311}, {'Grasper': 40917, 'Bipolar': 3742, 'Hook': 46880, 'Scissors': 1358, 'Clipper': 2525, 'Irrigator': 3830, 'SpecimenBag': 5877}, {'Grasper': 48912, 'Bipolar': 4365, 'Hook': 54979, 'Scissors': 1575, 'Clipper': 2896, 'Irrigator': 4434, 'SpecimenBag': 6424}, {'Grasper': 56580, 'Bipolar': 5112, 'Hook': 62952, 'Scissors': 1803, 'Clipper': 3341, 'Irrigator': 5150, 'SpecimenBag': 6975}, {'Grasper': 75621, 'Bipolar': 6769, 'Hook': 81135, 'Scissors': 2443, 'Clipper': 4399, 'Irrigator': 7007, 'SpecimenBag': 8585}]
 
 
 
 
-
-
-    """ folder = 'dinoV2_Cholec80_no_test_images/'
-    tool_counts_over_time = []
-    
-    for image_file in image_files:
-        images_file_path = os.path.join(folder, image_file)
-        tool_totals = process_images_file(images_file_path, tool_annotations_folder)
-        tool_counts_over_time.append(tool_totals)
-    
-    save_path = 'plots/dinoV2_Cholec80_no_test_images.png'
-    plot_tool_counts(tool_counts_over_time, save_path)
-
-
-
-
-    folder = 'endoViT_Cholec80_no_test_images/'
+    """ folder = 'endoViT_Cholec80_no_test_images/'
     tool_counts_over_time = []
     
     for image_file in image_files:
